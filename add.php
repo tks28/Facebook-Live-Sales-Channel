369b149c4ed998e6957de17689316b71e5932715
tks28 <tankuanshin28@gmail.com>
2022-08-23 20:47:50 +0800

    Added fucntion to add new item into inventory

---
+++ add.php
@@ -0,0 +1,23 @@
+<?php
+    require "config.php";
+    include('header.php');
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "facebook";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    $name = $_POST['itemName'];
+    $code = $_POST['itemCode'];
+    $price = $_POST['itemPrice'];
+    $quantity = $_POST['itemQuantity'];
+
+    $sql = "INSERT INTO inventory (name, code, price, quantity)
+    VALUES ('$name', '$code', '$price', '$quantity')";
+
+    $conn->query($sql);
+
+    header('Location: index.php');
+    
+?>
--- addItem.php
+++ addItem.php
@@ -4,3 +4,28 @@
     include('header.php');
 ?>
 
+<!doctype html>
+<html lang="en">
+    <body>
+        <form method="post" action="add.php">
+            <div class="form-group">
+                <label for="name">Item name</label>
+                <input type="text" class="form-control" name="itemName" placeholder="Enter item name">
+            </div>
+            <div class="form-group">
+                <label for="code">Item code</label>
+                <input type="text" class="form-control" name="itemCode" placeholder="Enter item code">
+            </div>
+            <div class="form-group">
+                <label for="price">Item price (RM)</label>
+                <input type="number" min="1" class="form-control" name="itemPrice" placeholder="Enter item price">
+            </div>
+            <div class="form-group">
+                <label for="quantity">Item quantity</label>
+                <input type="number" min="1" class="form-control" name="itemQuantity" placeholder="Enter item quantity">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </body>
+</html>
+
