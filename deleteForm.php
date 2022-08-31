546df4fb18375d5b5c7e1dbe167b58aa3ec54d67
tks28 <tankuanshin28@gmail.com>
2022-08-31 21:11:08 +0800

    Added delete function for inventory

---
+++ deleteForm.php
@@ -0,0 +1,46 @@
+<?php
+    require "config.php";
+    include('header.php');
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "facebook";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    $code = $_POST['itemCode'];
+
+    $sql = "SELECT code FROM inventory WHERE code='$code'";
+    $result = $conn->query($sql);
+
+    if($result->num_rows > 0){
+        echo "<h1>Are you sure you want to delete Item Code $code</h1>";
+    }
+    else{
+        echo "No result found";
+    }
+?>
+
+<!doctype html>
+<html lang="en">
+    <style>
+        h1 {
+            text-align: center;
+        }
+        .container {
+            text-align: center;
+        }
+    </style>
+
+    <body>
+        <div class="container">
+            <form method="POST" action="delete.php">
+                <input type="hidden" name="itemCode" value="<?php echo $code; ?>">
+                <button type="submit" class="btn btn-danger">Yes</button>
+            </form>
+            <a href="index.php">
+                <button type="button" class="btn btn-success">No</button>
+            </a>
+        </div>
+    </body>
+</html>
---
+++ deleteItem.php
@@ -0,0 +1,60 @@
+<?php
+	session_start();
+    $_SESSION['posted'] = "false";
+    include('header.php');
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "facebook";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+?>
+
+<table class="table table-striped">                     
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Code</th>
+            <th>Price</th>
+            <th>Quantity</th>
+        </tr>
+    </thead>
+    <tbody>
+<?php
+    $sql = "SELECT id, name, code, price, quantity FROM inventory";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $counter = 1;
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo '<tr>
+                    <td scope="row">' . $counter. '</td>
+                    <td>' . $row["name"] .'</td>
+                    <td> '.$row["code"] .'</td>
+                    <td> '.$row["price"] .'</td>
+                    <td> '.$row["quantity"] .'</td>
+                    </tr>';
+            $counter = $counter + 1;
+        }
+    } else {
+        echo "0 results";
+    } 
+?>
+    </tbody>
+</table>
+<!doctype html>
+<html lang="en">
+    <body>
+        <form method="post" action="deleteForm.php">
+            <div class="form-group">
+                <label for="code">Item code for delete</label>
+                <input type="text" class="form-control" name="itemCode" placeholder="Enter item code">
+            </div>
+            <button type="submit" class="btn btn-danger">Delete</button>
+        </form>
+    </body>
+</html>
+
