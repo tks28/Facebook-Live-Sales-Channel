921e7c22f0ee5afe80c045622b2722153e749543
tks28 <tankuanshin28@gmail.com>
2022-09-16 16:02:32 +0800

    Added Item quantity and stock count to live stream

--- live.php
+++ live.php
@@ -1,53 +1,86 @@
 <?php
-	session_start();
-  $_SESSION['posted'] = "false";
-  include('header.php');
+    session_start();
+    $_SESSION['posted'] = "false";
+    include('header.php');
 
-  $counter = 1;
-  $link = $_POST["link"];
+    $counter = 1;
+    $link = $_POST["link"];
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "facebook";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
 ?>
+
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Live</title>
-  </head>
-  <body>
-    <p align="center">
-      <?php echo $link; ?>
-    </p>
-    <div class="container text-center">
-      <div class="row">
-        <div class="col border">
-          Item
-        </div>
-        <div class="col border">
-          Status
-        </div>
-      </div>
-      <?php
-         $link = $_POST["link"];
-         if(!empty($_POST["items"])){
-           foreach($_POST["items"] as $item){
-             echo '<div class="row">
-                    <div class="col border">'
-                  .$item.
-                  '</div>
-                  <div class="col border">
-                  <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-                    <input type="radio" class="btn-check" name="btnradio" id="btnradio'.$counter.'">
-                    <label class="btn btn-outline-success" for="btnradio'.$counter.'">Select</label>';
-                      $counter = $counter +1;
-                    echo'<input type="radio" class="btn-check" name="btnradio" id="btnradio'.$counter.'">
-                    <label class="btn btn-outline-danger" for="btnradio'.$counter.'">Remove</label>
-                  </div>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Live</title>
+    </head>
+
+    <body>
+        <p align="center">
+            <?php echo $link; ?>
+        </p>
+
+        <div class="container text-center">
+            <div class="row">
+                <div class="col border">
+                    Item
                 </div>
-                </div>';
-                $counter = $counter + 1; 
-           }
-         }
-      ?>
-  </body>
+                <div class="col border">
+                    Action
+                </div>
+                <div class="col border">
+                    Item Quantity
+                </div>
+                <div class="col border">
+                    Current Stock Count
+                </div>
+            </div>
+            <?php
+                $link = $_POST["link"];
+
+                if(!empty($_POST["items"])){
+                    foreach($_POST["items"] as $item){
+                        echo '<div class="row">
+                            <div class="col border">'
+                            .$item.
+                            '</div>
+                            <div class="col border">
+                            <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                            <input type="radio" class="btn-check" name="btnradio" id="btnradio'.$counter.'">
+                            <label class="btn btn-outline-success" for="btnradio'.$counter.'">Select</label>';
+
+                            $counter = $counter +1;
+
+                        echo'<input type="radio" class="btn-check" name="btnradio" id="btnradio'.$counter.'">
+                            <label class="btn btn-outline-danger" for="btnradio'.$counter.'">Remove</label>
+                            </div>
+                            </div>';
+
+                        $sql = "SELECT quantity FROM inventory WHERE name='$item'";
+                        $result = $conn->query($sql);
+                        if ($result->num_rows > 0) {
+                            while($row = $result->fetch_assoc()) {
+                                
+                                echo'<div class="col border">'
+                                    .$row["quantity"].
+                                    '</div>';
+                                echo'<div class="col border">'
+                                    .$row["quantity"].
+                                    '</div>';
+                            }
+                        } 
+                        echo'</div>';
+                        $counter = $counter + 1; 
+                    }
+                }
+            ?>
+    </body>
 </html>
--- post.php
+++ post.php
@@ -18,14 +18,17 @@
             }
 
 ?>
-    <form method="post" action="page.php">
-        <div class="form-group col-md-6">
-            <label for="message">Message</label>
-            <textarea type="text" class="form-control" name="message" placeholder="Enter Message" rows="3"></textarea>
-        </div>
-        </br>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <div class="container">
+        <form method="post" action="page.php">
+            <div class="form-group col-md-6">
+                <label for="message">Post to Facebook</label>
+                <textarea type="text" class="form-control" name="message" placeholder="Enter Message" rows="3"></textarea>
+            </div>
+            </br>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+    
 <?php
     }catch(Facebook\Exceptions\FacebookSDKException $e){
         echo $e->getMessage();
